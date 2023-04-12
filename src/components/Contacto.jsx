c173d54d24f32b6389166fb74a4e4458bfdfbf53
Flor Salamanca <florsala@gmail.com>
2023-04-12 20:16:56 -0300

    cambios en contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,36 +1,33 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "../styles/Contacto.css";
-import ReactGA from 'react-ga';
-
-
+import ReactGA from "react-ga";
 
 import Mail from "../assets/mailbox.svg";
 
 const Contacto = () => {
-
-  const handleClick = () =>{
+  const handleClick = () => {
     ReactGA.event({
-      category: 'Button',
-      action: 'click botón contacto'
-    })
-    console.log('mensaje enviado');
-
-  }
-
+      category: "Button",
+      action: "click botón contacto",
+    });
+    console.log("mensaje enviado");
+  };
 
   const [inputNombre, cambiarInputNombre] = useState("");
+  const [inputEmpresa, cambiarInputEmpresa] = useState("");
+  const [inputWeb, cambiarInputWeb] = useState("");
+  const [inputRol, cambiarInputRol] = useState("");
+
   const [inputCorreo, cambiarInputCorreo] = useState("");
   const [inputMsg, cambiarInputMsg] = useState("");
   const [sentForm, setSentForm] = useState(false);
 
   const form = useRef();
 
-
   const sendEmail = (e) => {
     e.preventDefault();
     setSentForm(true);
-   
 
     emailjs
       .sendForm(
@@ -43,8 +40,12 @@ const Contacto = () => {
         (result) => {
           console.log(result.text);
           cambiarInputNombre("");
+          cambiarInputWeb("");
+
           cambiarInputCorreo("");
           cambiarInputMsg("");
+          cambiarInputEmpresa("");
+          cambiarInputRol("");
         },
         (error) => {
           console.log(error.text);
@@ -52,23 +53,30 @@ const Contacto = () => {
       );
   };
 
- 
-
   const handleInputNombre = (e) => {
     cambiarInputNombre(e.target.value);
+  };
+
+  const handleInputEmpresa = (e) => {
+    cambiarInputEmpresa(e.target.value);
   };
 
   const handleInputCorreo = (e) => {
     cambiarInputCorreo(e.target.value);
   };
+  const handleInputWeb = (e) => {
+    cambiarInputWeb(e.target.value);
+  };
 
- 
+  const handleInputRol = (e) => {
+    cambiarInputRol(e.target.value);
+  };
 
   const handleInputMsg = (e) => {
     cambiarInputMsg(e.target.value);
   };
   return (
-    <div id="contacto" >
+    <div id="contacto">
       <div className="Ct-container">
         <h2 className="Serv-title">¿Podemos ayudarte con algo más?</h2>
 
@@ -80,30 +88,6 @@ const Contacto = () => {
           <div className="Ct-container-text">
             <div className="Ct-container-head">
               <h3>¡Contactanos!</h3>
-            </div>
-          </div>
-
-          <form ref={form} onSubmit={sendEmail}  className="form-2">
-            <div style={{ display: "flex", alignItems: "baseline" }}>
-              <input
-                type="text"
-                name="nombre"
-                placeholder="Nombre"
-                id="nombre"
-                  value={inputNombre}
-            onChange={handleInputNombre} 
-                required
-              />
-
-              <input
-                type="text"
-                name="email"
-                placeholder="Email"
-                id="email"
-                value={inputCorreo}
-                onChange={handleInputCorreo} 
-                required
-              />
 
               <img
                 style={{
@@ -115,6 +99,64 @@ const Contacto = () => {
                 alt=""
               />
             </div>
+          </div>
+
+          <form ref={form} onSubmit={sendEmail} className="form-2">
+            <div style={{ display: "flex", alignItems: "baseline" }}>
+              <input
+                type="text"
+                name="nombre"
+                placeholder="Nombre"
+                id="nombre"
+                value={inputNombre}
+                onChange={handleInputNombre}
+                required
+              />
+
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                id="email"
+                value={inputCorreo}
+                onChange={handleInputCorreo}
+                required
+              />
+
+              
+            </div>
+            <div  style={{ display: "flex", alignItems: "baseline" }}>
+              <input
+                type="text"
+                name="empresa"
+                placeholder="Empresa"
+                id="empresa"
+                value={inputEmpresa}
+                onChange={handleInputEmpresa}
+                required
+              />
+
+              <input
+                type="text"
+                name="web"
+                placeholder="Web de la Empresa"
+                id="web"
+                value={inputWeb}
+                onChange={handleInputWeb}
+                required
+              />
+            </div>
+
+            <input
+              type="text"
+              name="rol"
+              placeholder="Rol de la persona"
+              id="rol"
+              value={inputRol}
+              onChange={handleInputRol}
+              required
+            />
+
             <div style={{ display: "flex" }}>
               <textarea
                 name="mensaje"
@@ -123,15 +165,18 @@ const Contacto = () => {
                 rows="1"
                 aria-required="true"
                 aria-invalid="false"
-                placeholder="Tu mensaje"
-                  value={inputMsg}
-            onChange={handleInputMsg} 
-                required
+                placeholder="Info adicional sobre rol/necesidad"
+                value={inputMsg}
+                onChange={handleInputMsg}
               />
 
-<input onClick={handleClick} className="btnForm" type="submit" value="Enviar"          
-            />
-{/*             {sentForm && ( <div style={{display:'flex', color:'white', alignItems:'center'}}> 
+              <input
+                onClick={handleClick}
+                className="btnForm"
+                type="submit"
+                value="Enviar"
+              />
+              {/*             {sentForm && ( <div style={{display:'flex', color:'white', alignItems:'center'}}> 
              
              <p className="exito"> 
              ¡Gracias por tu mensaje!</p></div>)
